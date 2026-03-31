d7eb99278eb19a47cc4f3615c3d046558251e395
huh12312 <huh12312@users.noreply.github.com>
2026-03-31 02:58:28 +0000

    fix: resolve mobile ESLint config - extend root config with mobile overrides

--- apps/mobile/.eslintrc.js
+++ apps/mobile/.eslintrc.js
@@ -1,21 +1,25 @@
 module.exports = {
-  extends: "expo",
-  ignorePatterns: ["/dist/*", "/node_modules/*"],
+  extends: "../../.eslintrc.js",
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  env: {
+    browser: false,
+    node: true,
+  },
   rules: {
-    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/no-floating-promises": "off",
     "@typescript-eslint/no-misused-promises": "off",
-    "@typescript-eslint/no-unsafe-enum-comparison": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/no-unsafe-member-access": "off",
     "@typescript-eslint/no-unsafe-call": "off",
     "@typescript-eslint/no-unsafe-return": "off",
     "@typescript-eslint/no-unsafe-argument": "off",
-    "@typescript-eslint/no-empty-object-type": "off",
-    "@typescript-eslint/no-wrapper-object-types": "off",
-    "@typescript-eslint/array-type": "off",
-    "react-hooks/exhaustive-deps": "warn",
+    "@typescript-eslint/no-unsafe-enum-comparison": "off",
     "import/namespace": "off",
     "import/no-unresolved": "off",
   },
+  ignorePatterns: ["/dist/*", "/node_modules/*", "*.config.js", "babel.config.js"],
 };
